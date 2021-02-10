ebb3e5be1c868f013f4c6bd1c8f123482ec4f170
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-02-10 10:20:20 +0100

    changed info and purchase box

--- product.js
+++ product.js
@@ -19,4 +19,10 @@ function showProduct(product) {
     "img.productimage"
   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
   document.querySelector("img.productimage").alt = product.productdisplayname;
+  document.querySelector("dt").textContent = product.productdisplayname;
+  document.querySelector(".price").textContent =
+    product.price + ",-" + " incl VAT";
+  document.querySelector(".purchaseBox p").textContent =
+    product.articletype + " | " + product.brandname;
+  //document.querySelector(".info p:nth-child(3)").textContent = product.description;
 }
